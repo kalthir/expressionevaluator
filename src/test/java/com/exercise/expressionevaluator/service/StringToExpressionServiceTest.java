259069568b0fc85e82a6e1159119adecfb23cf26
istimac <istimac@neogov.net>
2024-06-22 14:35:56 +0200

    Add tests for basic expressions with basic conditions

--- src/test/java/com/exercise/expressionevaluator/service/StringToExpressionServiceTest.java
+++ src/test/java/com/exercise/expressionevaluator/service/StringToExpressionServiceTest.java
@@ -7,10 +7,30 @@ import static org.junit.jupiter.api.Assertions.*;
 class StringToExpressionServiceTest {
 
     @Test
-    void stringToExpression() {
+    void stringToExpressionExampleTest() {
         StringToExpressionService stringToExpressionService = new StringToExpressionService();
         try {
             stringToExpressionService.stringToExpression("(customer.firstName == \"JOHN\" && customer.salary < 100) OR (customer.address != null && customer.address.city == \"Washington\")");
+        } catch (Exception e) {
+            fail("Exception thrown");
+        }
+    }
+
+    @Test
+    void stringToExpressionBasicConditionStringTest() {
+        StringToExpressionService stringToExpressionService = new StringToExpressionService();
+        try {
+            stringToExpressionService.stringToExpression("customer.firstName == \"JOHN\"");
+        } catch (Exception e) {
+            fail("Exception thrown");
+        }
+    }
+
+    @Test
+    void stringToExpressionBasicConditionNumberTest() {
+        StringToExpressionService stringToExpressionService = new StringToExpressionService();
+        try {
+            stringToExpressionService.stringToExpression("customer.salary < 100");
         } catch (Exception e) {
             fail("Exception thrown");
         }
